a1a57e9944509bd5ed063c9b74b87957a4d57cf8
Joao Pedro linhares <joaopedrolinhares@Joaos-Laptop.local>
2025-10-09 00:32:20 +0200

    adding main.js

---
+++ staff/joao-oliveira/product/app/main.js
@@ -0,0 +1,66 @@
+//register
+
+var registerView = document.querySelector('#registerView')
+registerView.style.display = 'none'
+
+var registerForm = registerView.querySelector('#registerForm')
+
+registerForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var fullName = registerForm.fullName.value
+    var dateOfBirth = registerForm.dateOfBirth.value
+    var email = registerForm.email.value
+    var password = registerForm.password.value
+
+    logic.registerUser(fullName, dateOfBirth, email, password)
+
+    registerForm.reset()
+
+    registerView.style.display = 'none'
+    loginView.style.display = 'block'
+})
+
+var loginLink = registerView.querySelector('#loginLink')
+
+loginLink.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    registerView.style.display = 'none'
+    loginView.style.display = 'block'
+})
+
+//login
+
+
+var loginView = document.querySelector('#loginView')
+
+var loginForm = loginView.querySelector('#loginForm')
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = loginForm.email.value
+    var password = loginForm.password.value
+
+    logic.loginUser(email, password)
+
+    logic.form.reset()
+
+    loginView.style.display = 'none'
+    homeView.style.display = 'block'
+})
+
+var registerLink = loginView.querySelector('#registerLink')
+
+registerLink.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    loginView.style.display = 'none'
+    registerView.style.display = 'block'
+})
+
+//home 
+
+var homeView = document.querySelector('#homeView')
+homeView.style.display = 'none'
